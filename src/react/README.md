abf92390a5e3a2a5b59ffa31b40bd3326c470502
LQQ <liqianqian01@haier.com>
2021-04-02 21:13:32 +0800

    add react 源码

---
+++ src/react/README.md
@@ -0,0 +1,3 @@
+# [React](https://reactjs.org/)  ![npm version](https://img.shields.io/npm/v/react.svg?style=flat)
+
+> 版本：17.0.2
---
+++ src/react/packages/react/src/ReactCreateRef.js
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ * @flow
+ */
+
+import type {RefObject} from 'shared/ReactTypes';
+
+// an immutable object with a single mutable value
+// 默认情况下：函数组件是无法使用 ref，因为他们没有实例。
+export function createRef(): RefObject {
+  const refObject = {
+    current: null,
+  };
+  if (__DEV__) {
+    Object.seal(refObject);
+  }
+  return refObject;
+}
